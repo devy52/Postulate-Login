e90b9d7f0bf6abcdb0b715a9050abc522ddb214f
devy52 <108571763+devy52@users.noreply.github.com>
2023-06-23 19:40:05 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import theme from "theme";
-import { Theme, Link, Text, List, Box, Section } from "@quarkly/widgets";
+import { Theme, Link, Text, List, Box, Section, Icon } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
 import { RawHtml } from "@quarkly/components";
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
+import "./index.css";
 export default (() => {
+	const [showPassword, setShowPassword] = useState(false);
+  const [rippleList, setRippleList] = useState([]);
+
+  const createRipple = (e) => {
+    const button = e.currentTarget;
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    const newRipple = {
+      left: e.clientX - button.getBoundingClientRect().left - radius + "px",
+      top: e.clientY - button.getBoundingClientRect().top - radius + "px",
+      diameter: diameter + "px"
+    };
+
+    setRippleList((prevRipples) => [...prevRipples, newRipple]);
+  };
+
+
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
 		<Helmet>
@@ -13,6 +33,9 @@ export default (() => {
 			</title>
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+			<link rel={"preconnect"} href={"https://fonts.googleapis.com"}/>
+			<link rel={"preconnect"} href={"https://fonts.gstatic.com"} crossorigin/>
+			<link href={"https://fonts.googleapis.com/css2?family=Diphylleia&family=Teko&display=swap"} rel={"stylesheet"}/>
 		</Helmet>
 		<Section padding="0px 0 0px 0">
 			<Box
@@ -76,10 +99,102 @@ export default (() => {
 					>
 						LOGIN
 					</Link>
+					<Link
+						href="#"
+						color="white"
+						padding="6px 12px 6px 12px"
+						margin-left="0.4em"
+						text-decoration-line="initial"
+						display="flex"
+						font="30px/30px --fontFamily-googleTeko"
+						hover-color="--lightD2"
+						transition="background-color 0.1s ease 0s"
+						background="#3468d3"
+						border-style="solid"
+						border-width="2px"
+						hover-background="#898c96"
+					>
+						REGISTER
+					</Link>
 				</List>
 			</Box>
 		</Section>
-		<Section background="url(https://wallpaperaccess.com/full/1905877.jpg) 20% 15%/cover" padding="60px 0" sm-padding="40px 0" height="556px" />
+		<Section background="url(https://wallpaperaccess.com/full/1905877.jpg) 20% 15%/cover" padding="60px 0" sm-padding="40px 0" height="556px">
+  			<Box
+    			display="flex"
+    			justify-content="center"
+    			align-items="center"
+    			height="100%"
+  			>
+    		<form>
+				
+      		<Box
+        		display="flex"
+        		flex-direction="column"
+        		padding="40px"
+        	border-radius="8px"
+        	width="400px"
+			className="frmbox"
+      		>
+			<label className="head1">Login</label>
+			<label className="mail">Email</label>
+        	<input
+          	type="email"
+          	placeholder="Email"
+          	margin-bottom="20px"
+          	padding="10px"
+          	border-radius="4px"
+          	border="1px solid #cccccc"
+			className="mailin"
+        	/>
+			<label className="pass">Password</label>
+        	<input
+          	type={showPassword ? "text" : "password"}
+          	placeholder="Password"
+          	margin-bottom="20px"
+          	padding="10px"
+          	border-radius="4px"
+          	border="1px solid #cccccc"
+			height={"200"}
+			className="passe"
+        	/>
+			<Icon
+              category="md"
+              icon={showPassword ? <MdVisibilityOff /> : <MdVisibility />}
+              size="24px"
+              color="#f70a0a"
+              position="absolute"
+              right="4px"
+              top="70%"
+              transform="translateY(-50%)"
+              onClick={() => setShowPassword((prevShowPassword) => !prevShowPassword)}
+              cursor="pointer"
+			  className="icon visible-icon"
+            />
+        	<button
+        type="submit"
+        className="ripple-btn"
+        onClick={createRipple}
+      >
+        Login
+        {rippleList.map((ripple, index) => (
+          <span
+            key={index}
+            style={{
+              width: ripple.diameter,
+              height: ripple.diameter,
+              left: ripple.left,
+              top: ripple.top
+            }}
+            className="ripple"
+          />
+        ))}
+      </button>
+      		</Box>
+    		</form>
+  			</Box>
+		</Section>
+
 		<Section background-color="--dark" text-align="center" padding="32px 0" quarkly-title="Footer-1">
 			<List
 				margin="0px 0px 0px 0px"
