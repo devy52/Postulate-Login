ac0a060ab05d76bb633bc7a90d7b453ba8eaff75
devy52 <108571763+devy52@users.noreply.github.com>
2023-06-26 13:39:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Index from "./pages/index";
 import Page404 from "./pages/page404";
 import Signup from "./pages/signup";
+import Home from "./pages/home";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 
@@ -20,6 +21,7 @@ const App = () => (
       <Route path="/" exact component={Index} />
       <Route path="/index" component={Index} />
       <Route path="/signup" component={Signup} />
+      <Route path="/home" component={Home} />
       <Route component={Page404} />
       <Redirect to="/" />
     </Switch>
